7f6b3c5b095a45840c57c1594e196ae2e10b657e
liquid0624 <rjbirnesser@gmail.com>
2017-08-09 01:53:03 -0400

    op5: make our deso cheeseburger
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- deso.mk
+++ deso.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The MoKee Open Source Project
+# Copyright (C) 2017 DesolationROM
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -21,11 +21,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from cheeseburger device
 $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 
-# Inherit some common Carbon stuff.
-$(call inherit-product, vendor/carbon/config/common.mk)
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+# Inherit some common Deso stuff.
+$(call inherit-product, vendor/deso/common.mk)
 
-PRODUCT_NAME := carbon_cheeseburger
+PRODUCT_NAME := deso_cheeseburger
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -41,8 +40,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=OnePlus5 PRODUCT_NAME=OnePlus5
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=OnePlus/OnePlus5/OnePlus5:7.1.1/NMF26X/06241119:user/release-keys
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carbon.maintainer="gtpitch"
-
 TARGET_VENDOR := oneplus
-
