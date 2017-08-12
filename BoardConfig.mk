e1a3e2a8d6c5606580dedb38c370d42c7bbda0fd
liquid0624 <rjbirnesser@gmail.com>
2017-08-11 21:19:49 -0400

    op5: build kernel inline with linaro 7.x
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -71,7 +71,8 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := cheeseburger_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linaro-linux-android-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-7.x/bin
 
 # HAX: Remove AOSP
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
